f059f648b3c0f584f0a5930fc4a7840323b0b7da
Yliceeee <ericaylicevelez@gmail.com>
2024-10-07 09:16:16 +0800

    Create 02BasicOutput.cpp

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	string school;
+	string department;
+	string courseandyear;
+	string name;
+	string adress;
+	
+	cout << "School\t";
+	cin >> school;
+	cout << "Department\t";
+	cin >> department;
+	cout << "Course & Year\t";
+	cin >> courseandyear;
+	cout << "Your full name\t";
+	cin >> name;
+	cout << "Your Permanent Adress\t";
+	cin >> adress;
+	
+	return 0;
+}
